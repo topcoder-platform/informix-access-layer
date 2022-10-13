b7db3c610fe64068aa5a54b91c93b2d0fe94e9b0
Rakib Ansary <rakibansary@gmail.com>
2022-10-13 20:14:13 +0600

    first commit with base implementation

---
+++ src/main/java/com/topcoder/dal/DBAccessor.java
@@ -0,0 +1,188 @@
+package com.topcoder.dal;
+
+import com.topcoder.dal.rdb.*;
+import com.topcoder.dal.util.IdGenerator;
+import com.topcoder.dal.util.QueryHelper;
+import io.grpc.stub.StreamObserver;
+import jdk.jshell.spi.ExecutionControl;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Accessor for rational database like Informix.
+ */
+@GrpcService
+public class DBAccessor extends QueryServiceGrpc.QueryServiceImplBase {
+
+    /**
+     * JDBC template.
+     */
+    private final JdbcTemplate jdbcTemplate;
+
+    private final QueryHelper queryHelper;
+
+    private final IdGenerator idGenerator;
+
+    public DBAccessor(JdbcTemplate jdbcTemplate, QueryHelper queryHelper, IdGenerator idGenerator) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.queryHelper = queryHelper;
+        this.idGenerator = idGenerator;
+    }
+
+    /**
+     * Execute query operation.
+     *
+     * @param query            The query clause, in format "from ... where ..."
+     * @param params           The parameters to bind to query, may be null
+     * @param returningColumns The columns to be returned
+     * @return query result
+     * @throws DataAccessException
+     */
+    public List<String[]> executeQuery(String query, String[] params, String[] returningColumns)
+            throws DataAccessException {
+        String sql = "select " + String.join(",", returningColumns) + " " + query;
+
+        return jdbcTemplate.query(sql, (rs, _rowNum) -> {
+            String[] rowResult = new String[returningColumns.length];
+
+            for (int idx = 0; idx < returningColumns.length; idx++) {
+                Object value = rs.getObject(idx + 1);
+                rowResult[idx] = value == null ? null : value.toString();
+            }
+            return rowResult;
+        }, (Object[]) params);
+    }
+
+    /**
+     * Execute update operation.
+     *
+     * @param query  The query clause
+     * @param params The parameters to bind to query, may be null
+     * @return the number of rows affected
+     * @throws DataAccessException
+     */
+    public int executeUpdate(String query, String[] params) throws DataAccessException {
+        return jdbcTemplate.update(query, (Object[]) params);
+    }
+
+    /**
+     * Execute batch update operations in transaction.
+     *
+     * @param query The query clauses
+     * @return the number of rows affected
+     * @throws DataAccessException
+     */
+    @Transactional
+    public int[] executeBatchUpdate(String[] query, String[][] params) throws DataAccessException {
+        int size = query.length;
+        if (size != params.length) {
+            throw new IllegalArgumentException("Query array and params array must have same length");
+        }
+        int[] result = new int[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = jdbcTemplate.update(query[i], params == null ? null : (Object[]) params[i]);
+        }
+        return result;
+    }
+
+    @Override
+    public void query(QueryRequest request, StreamObserver<QueryResponse> responseObserver) {
+        Query query = request.getQuery();
+        QueryResponse response = null;
+
+        switch (query.getQueryCase()) {
+            case RAW -> {
+                // TODO: Handle Raw
+                responseObserver.onError(new ExecutionControl.NotImplementedException("Raw query is not implemented"));
+                return;
+            }
+            case SELECT -> {
+                final SelectQuery selectQuery = query.getSelect();
+                final String sql = queryHelper.getSelectQuery(selectQuery);
+                final List<Column> columnList = selectQuery.getColumnList();
+                final int numColumns = columnList.size();
+                final ColumnType[] columnTypeMap = new ColumnType[numColumns];
+                for (int i = 0; i < numColumns; i++) {
+                    columnTypeMap[i] = columnList.get(i).getType();
+                }
+                List<Row> rows = jdbcTemplate.query(sql, (rs, rowNum) -> {
+                    Row.Builder rowBuilder = Row.newBuilder();
+                    Value.Builder valueBuilder = Value.newBuilder();
+
+                    for (int i = 0; i < numColumns; i++) {
+                        switch (columnTypeMap[i]) {
+                            case INT -> valueBuilder.setIntValue(rs.getInt(i + 1));
+                            case LONG -> valueBuilder.setLongValue(rs.getLong(i + 1));
+                            case FLOAT -> valueBuilder.setFloatValue(rs.getFloat(i + 1));
+                            case DOUBLE -> valueBuilder.setDoubleValue(rs.getDouble(i + 1));
+                            case STRING ->
+                                    valueBuilder.setStringValue(Objects.requireNonNullElse(rs.getString(i + 1), ""));
+                            case BOOLEAN -> valueBuilder.setBooleanValue(rs.getBoolean(i + 1));
+                            case DATE, DATETIME -> valueBuilder.setDateValue(rs.getTimestamp(i + 1).toString());
+                            default ->
+                                    throw new IllegalArgumentException("Unsupported column type: " + columnTypeMap[i]);
+                        }
+
+                        rowBuilder.putValues(columnList.get(i).getName(), valueBuilder.build());
+                    }
+                    return rowBuilder.build();
+                });
+                response = QueryResponse.newBuilder()
+                        .setSelectResult(SelectQueryResult.newBuilder().addAllRows(rows).build())
+                        .build();
+            }
+            case INSERT -> {
+                final InsertQuery insertQuery = query.getInsert();
+                final boolean shouldGenerateId = insertQuery.hasIdSequence() && (insertQuery.hasIdColumn() || insertQuery.hasIdTable());
+
+                final String sql;
+                long id = 0;
+                if (shouldGenerateId) {
+                    final String idColumn = insertQuery.getIdColumn();
+                    final String idSequence = insertQuery.getIdSequence();
+                    id = idSequence.equalsIgnoreCase("MAX") ? idGenerator.getMaxId(insertQuery.getIdTable(), insertQuery.getIdColumn()) : idGenerator.getNextId(idSequence);
+                    sql = queryHelper.getInsertQuery(insertQuery, idColumn, String.valueOf(id));
+                } else {
+                    sql = queryHelper.getInsertQuery(insertQuery);
+                }
+
+                System.out.println("Insert SQL: " + sql);
+                jdbcTemplate.update(sql);
+
+                InsertQueryResult.Builder insertQueryBuilder = InsertQueryResult.newBuilder();
+                if (shouldGenerateId) {
+                    insertQueryBuilder.setLastInsertId(id);
+                }
+
+                response = QueryResponse.newBuilder()
+                        .setInsertResult(insertQueryBuilder.build())
+                        .build();
+            }
+            case UPDATE -> {
+                final UpdateQuery updateQuery = query.getUpdate();
+                final String sql = queryHelper.getUpdateQuery(updateQuery);
+                final int updateCount = jdbcTemplate.update(sql);
+                response = QueryResponse.newBuilder()
+                        .setUpdateResult(UpdateQueryResult.newBuilder().setAffectedRows(updateCount).build())
+                        .build();
+            }
+            case DELETE -> {
+                final DeleteQuery deleteQuery = query.getDelete();
+                final String sql = queryHelper.getDeleteQuery(deleteQuery);
+                final int deleteCount = jdbcTemplate.update(sql);
+                response = QueryResponse.newBuilder()
+                        .setDeleteResult(DeleteQueryResult.newBuilder().setAffectedRows(deleteCount).build())
+                        .build();
+            }
+        }
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+}
---
+++ src/main/java/com/topcoder/dal/util/QueryHelper.java
@@ -0,0 +1,158 @@
+package com.topcoder.dal.util;
+
+import com.topcoder.dal.rdb.*;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+@Component
+public class QueryHelper {
+
+    public String getSelectQuery(SelectQuery query) {
+        final String tableName = query.getTable();
+
+        List<Column> columnsList = query.getColumnList();
+
+        final String[] columns = columnsList.stream()
+                .map(Column::getName)
+                .toArray(String[]::new);
+
+        final String[] whereClause = query.getWhereList().stream()
+                .map(toWhereCriteria)
+                .toArray(String[]::new);
+
+        final String[] groupByClause = query.getGroupByList().toArray(new String[0]);
+        final String[] orderByClause = query.getOrderByList().toArray(new String[0]);
+
+        final int limit = query.getLimit();
+        final int offset = query.getOffset();
+
+        // SELECT SKIP 10 FIRST 5 * FROM table // informix
+        // SELECT * FROM table LIMIT 5 OFFSET 10; // postgres
+
+        return "SELECT"
+                + (offset > 0 ? " SKIP " + offset : "")
+                + (limit > 0 ? " FIRST " + limit : "")
+                + (" " + String.join(",", columns) + " FROM " + tableName)
+                + (whereClause.length > 0 ? " WHERE " + String.join(" AND ", whereClause) : "")
+                + (groupByClause.length > 0 ? " GROUP BY " + String.join(",", groupByClause) : "")
+                + (orderByClause.length > 0 ? " ORDER BY " + String.join(",", orderByClause) : "");
+    }
+
+    public String getInsertQuery(InsertQuery query) {
+        return getInsertQuery(query, null, null);
+    }
+
+    public String getInsertQuery(InsertQuery query, String idColumn, String idValue) {
+        final String tableName = query.getTable();
+        final List<ColumnValue> valuesToInsert = query.getColumnValueList();
+
+        Stream<String> columnsStream = valuesToInsert.stream()
+                .map(ColumnValue::getColumn);
+
+        Stream<String> valueStream = valuesToInsert.stream()
+                .map(ColumnValue::getValue)
+                .map(QueryHelper::toValue);
+
+        final String[] columns;
+        final String[] values;
+
+        if (query.hasIdColumn() && query.hasIdSequence()) {
+            columns = Stream.concat(Stream.of(idColumn), columnsStream).toArray(String[]::new);
+            values = Stream.concat(Stream.of(idValue), valueStream).toArray(String[]::new);
+        } else {
+            columns = columnsStream.toArray(String[]::new);
+            values = valueStream.toArray(String[]::new);
+        }
+
+        return "INSERT INTO " + tableName + " (" + String.join(",", columns) + ") VALUES (" + String.join(",", values) + ")";
+    }
+
+
+    public String getUpdateQuery(UpdateQuery query) {
+        final String tableName = query.getTable();
+
+        final List<ColumnValue> valuesToUpdate = query.getColumnValueList();
+        final String[] columns = valuesToUpdate.stream().map(ColumnValue::getColumn).toArray(String[]::new);
+        final String[] values = valuesToUpdate.stream().map(ColumnValue::getValue).map(QueryHelper::toValue).toArray(String[]::new);
+
+        final String[] whereClause = query.getWhereList().stream()
+                .map(toWhereCriteria)
+                .toArray(String[]::new);
+
+        if (whereClause.length == 0) {
+            throw new RuntimeException("Update query must have a where clause");
+        }
+
+        return "UPDATE "
+                + tableName
+                + " SET " + String.join(",", zip(columns, values, (c, v) -> c + "=" + v))
+                + " WHERE " + String.join(" AND ", whereClause);
+    }
+
+    public String getDeleteQuery(DeleteQuery query) {
+        final String tableName = query.getTable();
+
+        final String[] whereClause = query.getWhereList().stream()
+                .map(toWhereCriteria)
+                .toArray(String[]::new);
+
+        if (whereClause.length == 0) {
+            throw new IllegalArgumentException("Delete query must have a where clause");
+        }
+
+        return "DELETE FROM "
+                + tableName
+                + " WHERE " + String.join(" AND ", whereClause);
+    }
+
+    private static final Function<WhereCriteria, String> toWhereCriteria = (criteria) -> {
+        String key = criteria.getKey();
+        String value = toValue(criteria.getValue());
+
+        return switch (criteria.getOperator()) {
+            case EQUAL -> key + "=" + value;
+            case NOT_EQUAL -> key + "<>" + value;
+            case GREATER_THAN -> key + ">" + value;
+            case GREATER_THAN_OR_EQUAL -> key + ">=" + value;
+            case LESS_THAN -> key + "<" + value;
+            case LESS_THAN_OR_EQUAL -> key + "<=" + value;
+            case LIKE -> key + " LIKE " + value;
+            case NOT_LIKE -> key + " NOT LIKE " + value;
+            case IN -> key + " IN (" + value + ")";
+            case NOT_IN -> key + " NOT IN (" + value + ")";
+            case IS_NULL -> key + " IS NULL";
+            case IS_NOT_NULL -> key + " IS NOT NULL";
+            default -> null;
+        };
+    };
+
+
+    private static String toValue(Value value) {
+        return switch (value.getValueCase()) {
+            case STRING_VALUE -> "'" + value.getStringValue() + "'";
+            case INT_VALUE -> String.valueOf(value.getIntValue());
+            case LONG_VALUE -> String.valueOf(value.getLongValue());
+            case DOUBLE_VALUE -> String.valueOf(value.getDoubleValue());
+            case FLOAT_VALUE -> String.valueOf(value.getFloatValue());
+            case BOOLEAN_VALUE -> String.valueOf(value.getBooleanValue());
+            case DATE_VALUE -> value.getDateValue();
+            case DATETIME_VALUE -> value.getDatetimeValue();
+            case BLOB_VALUE, VALUE_NOT_SET -> null;
+        };
+    }
+
+    private static String[] zip(String[] columns, String[] values, BiFunction<String, String, String> f) {
+        final int length = columns.length;
+        final String[] result = new String[length];
+
+        for (int i = 0; i < length; i++) {
+            result[i] = f.apply(columns[i], values[i]);
+        }
+
+        return result;
+    }
+}
