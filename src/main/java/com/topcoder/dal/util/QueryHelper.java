1dd8c2a55557134fec8eb6236ad698314526e784
Rakib Ansary <rakibansary@topcoder.com>
2023-07-05 16:52:55 +0600

    fix: allow + and * in sql statements (#15)
    * fix: allow + and * in sql statements
    
    Signed-off-by: Rakib Ansary <rakibansary@topcoder.com>
    
    * fix: allow ' in sql statements
    
    ---------
    
    Signed-off-by: Rakib Ansary <rakibansary@topcoder.com>

--- src/main/java/com/topcoder/dal/util/QueryHelper.java
+++ src/main/java/com/topcoder/dal/util/QueryHelper.java
@@ -171,7 +171,7 @@ public class QueryHelper {
         StringBuilder safeSQL = new StringBuilder();
         for (char c : sql.toCharArray()) {
             if (Character.isLetterOrDigit(c) || c == ' ' || c == ',' || c == '(' || c == ')' || c == '=' || c == '<'
-                    || c == '>' || c == '_' || c == ':' || c == '.' || c == '-') {
+                    || c == '>' || c == '_' || c == ':' || c == '.' || c == '-' || c == '+' || c == '*' || c == '\'') {
                 safeSQL.append(c);
             }
         }
